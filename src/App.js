86fc94b5ac052ce024f8ecf95c0ce6df3c72db1a
Undong00 <ehddjs85246@gmail.com>
2023-09-21 03:25:56 +0900

    feat : 나의 학습방 진행률 기능 및 css 구축

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Progress from "./components/Progress";
 function App() {
-  return <div>Hello, World!</div>;
+  // return <div>Hello, World!</div>;
+  return (
+    <div>
+      <Progress />
+    </div>
+  );
 }
 
 export default App;
