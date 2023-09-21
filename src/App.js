3d33c1b0025d7c68d76cd82a579888422523148e
Undong00 <ehddjs85246@gmail.com>
2023-09-21 18:12:46 +0900

    #5 feat : 오늘의 과제 및 찜목록 기능 연결 및 검사

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Router from "./shared/Router";
+// import Router from "./shared/Router";
 import "./style/style.css";
 
+import Wishlist from "./components/Wishlist";
+
 function App() {
-  return <Router />;
+  // return <Router />;
+  return <Wishlist />;
 }
 
 export default App;
