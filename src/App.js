58f068bb675c45d27067fe1edb29d56d6d90c4d0
minnjeong <bbono4500@gmail.com>
2023-09-23 02:14:27 +0900

    feat:Modal 출석체크 달력 api 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import Router from "./shared/Router";
-// import Router from "./shared/Router";
-import "./style/style.css";
-
-import Wishlist from "./components/Wishlist";
 
 function App() {
-  // return <Router />;
-  return <Wishlist />;
+  return <Router />;
 }
 
 export default App;
