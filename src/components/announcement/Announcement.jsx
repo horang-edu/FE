ce2f58dbffd996175102e1fc0716f1600d16682a
minnjeong <bbono4500@gmail.com>
2023-09-23 14:32:23 +0900

    #24 design:메인 CSS

---
+++ src/components/announcement/Announcement.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Announcement() {
+  return (
+    <>
+    <div>
+        <div>1</div>
+        <div>2학기 수업 커리큘럼</div>
+        <div>23/09/01</div>
+    </div>
+    <div>
+        <div>2</div>
+        <div>수업 자료 다운로드 설명서 첨부합니다.</div>
+        <div>23/09/14</div>
+    </div>
+    <div>
+        <div>3</div>
+        <div>과제 공지입니다.</div>
+        <div>23/09/16</div>
+    </div>
+    </>
+  )
+}
+
+export default Announcement
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Tab from "../components/tab/Tab";
 import UserRanking from "../components/ranking/UserRanking";
 import SchoolRanking from "../components/ranking/SchoolRanking";
 import CampusRanking from "../components/ranking/CampusRanking";
 import CalendarMission from "../components/mission/CalendarMission";
 import Mission from "../components/mission/Mission";
+import Announcement from "../components/announcement/Announcement";
+import QA from "../components/announcement/QA";
+import Modal from "../components/mission/Modal";
 
 function Home() {
+  const [showModal, setShowModal] = useState(false);
   const tabData = [
-    { menu: "사용자 순위", content: <UserRanking />, type: "userRanking" },
-    { menu: "학교 별 순위", content: <SchoolRanking />, type: "schoolRanking" },
-    { menu: "학교 내 순위", content: <CampusRanking />, type: "campusRanking" },
+    { menu: "사용자 순위", content: <UserRanking /> ,type:'userRank'},
+    { menu: "학교 별 순위", content: <SchoolRanking />,type:'schoolRank' },
+    { menu: "학교 내 순위", content: <CampusRanking />,type:'campusRank' },
   ];
 
   const announcement = [
-    { menu: "공지사항", content: <SchoolRanking />, type: "schoolRanking" },
-    { menu: "Q & A", content: <UserRanking />, type: "userRanking" },
-  ];
-  const announcement = [
-    { menu: "공지사항", content: <UserRanking /> },
-    { menu: "Q & A", content: <UserRanking /> },
+    { menu: "공지사항", content: <Announcement />, type: 'notice' },
+    { menu: "Q & A", content: <QA />, type: 'question' },
   ]
 
   return (
@@ -27,18 +27,20 @@ function Home() {
       <div>
         안녕하세요, <span>코딩짱호랭이</span>님!
       </div>
-      <div>
-        <div>공지사항</div>
-        <Tab tabData={announcement} />
+      <div class="w-[62.1875rem]">
+        <Tab tabData={announcement}/>
       </div>
       <div class='flex'>
         <CalendarMission/>
-        <Mission/>
+        <Mission showModal={showModal} setShowModal={setShowModal}/>
       </div>
+      {showModal && (
+        <Modal show={showModal} onClose={() => setShowModal(false)} />
+      )}
       <div>새로 업데이트 된 문제</div>
       <div className="w-[33.875rem]">
         <h2>순위 차트</h2>
-        <Tab data={tabData} />
+        <Tab tabData={tabData} />
       </div>
     </div>
   );
