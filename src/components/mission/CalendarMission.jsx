042a1e6e861dccdd5ee339af21990ce65045caec
v-va <poruch0915@gmail.com>
2023-09-24 08:19:28 +0900

    fix : mission 폴더 삭제 후 재 업로드

---
+++ src/components/mission/CalendarMission.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Stamp from "../../assets/svg/Stamp.svg";
+
+function CalendarMission({ showModal, setShowModal }) {
+    
+    const handleClick = () => {
+        setShowModal(true);
+        };
+        
+  return (
+    <div onClick={handleClick} class="mission-layout w-417 flex flex-row bg-color2 relative shadow-custom mr-[2.125rem] cursor-pointer">
+      <img class="absolute top-0 bottom-0 left-1/3 transform -translate-x-1/8 w-1/4 h-auto" src={Stamp} alt="stamp" />
+      <div class="flex flex-col pl-25 pt-16 pb-19 justify-start relative">
+        <div class="font-semibold text-color6 text-24">9월</div>
+        <div class="font-semibold text-22 mb-10">매일 출석체크</div>
+        <div class="font-medium text-16">
+          꾸준히 코딩 학습하고 <span class="font-semibold">경험치</span>도 받아요!
+        </div>
+      </div>
+    </div>
+  );
+
+}
+export default CalendarMission;
