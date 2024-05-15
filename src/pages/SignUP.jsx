9e3aeda5f85a4330b7085e2116990880bf81ce27
이동언 <undong@idong-eon-ui-MacBookPro.local>
2024-05-15 20:10:05 +0900

    #61 feat : 회원가입 기능 구현

--- src/pages/SignUP.jsx
+++ src/pages/SignUP.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function SignUp() {
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isTeacher, setIsTeacher] = useState(false);
@@ -10,8 +11,8 @@ function SignUp() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!username || !password) {
-      alert("이름과 비밀번호를 입력해주세요.");
+    if (!name || !email || !password) {
+      alert("이름, 이메일, 비밀번호를 입력해주세요.");
       return;
     }
     if (password !== confirmPassword) {
@@ -20,18 +21,22 @@ function SignUp() {
     }
 
     const userData = {
-      username,
+      name,
+      email,
       password,
-      role: isTeacher, // 'role' is set based on 'isTeacher'
+      role: isTeacher,
     };
 
     try {
-      const response = await axios.post("https://example.com/api/signup", userData);
+      const response = await axios.post(
+        "http://13.209.48.232:8080/api/user/signup",
+        userData
+      );
       console.log("Response:", response);
-      alert("Signup successful!");
+      alert("회원가입 완료!");
     } catch (error) {
       console.error("Signup error:", error);
-      alert("An error occurred during signup.");
+      alert("회원가입 실패!");
     }
   };
 
@@ -41,42 +46,58 @@ function SignUp() {
         <div className="absolute top-[-81px] left-[46%]">
           <button
             onClick={() => setIsTeacher(false)}
-            style={{ fontWeight: !isTeacher ? "bold" : "normal" }}
-            className="w-[100px] h-[54px] bg-[white] rounded-[20px] border-solid border-[1px] border-[#FFD7C3] font-yg-jalnan text-[#F99363] text-[18px]"
+            style={{
+              fontWeight: !isTeacher ? "bold" : "normal",
+              backgroundColor: !isTeacher ? "#F99363" : "#FFF8EF",
+              color: !isTeacher ? "white" : "#F99363",
+              borderColor: "#F99363",
+            }}
+            className="w-[100px] h-[54px] rounded-[20px] border-solid border-[1px] font-yg-jalnan text-[18px] hover:border-[#F99363]"
           >
             학생
           </button>
           <button
             onClick={() => setIsTeacher(true)}
-            style={{ fontWeight: isTeacher ? "bold" : "normal" }}
-            className="w-[100px] h-[54px] bg-[white] rounded-[20px] border-solid border-[1px] border-[#FFD7C3] font-yg-jalnan text-[#F99363] text-[18px] ml-[10px]"
+            style={{
+              fontWeight: isTeacher ? "bold" : "normal",
+              backgroundColor: isTeacher ? "#F99363" : "#FFF8EF",
+              color: isTeacher ? "white" : "#F99363",
+              borderColor: "#F99363",
+            }}
+            className="w-[100px] h-[54px] rounded-[20px] border-solid border-[1px] ml-[10px] font-yg-jalnan text-[18px] hover:border-[#F99363]"
           >
             선생님
           </button>
         </div>
-        {/* <div className="flex justify-between">
-          <div className="flex justify-center items-center">
-            <div onClick={() => setIsTeacher(false)} style={{ fontWeight: !isTeacher ? "bold" : "normal" }}>
-              학생
-            </div>
-            <div onClick={() => setIsTeacher(true)} style={{ fontWeight: isTeacher ? "bold" : "normal" }}>
-              선생님
-            </div>
-          </div>
-        </div> */}
-        <div
+        <form
           className="border"
           style={{
             width: "391px",
-            height: "553px",
+            height: "613px",
             padding: "77px 33px 77px 33px",
             borderColor: "#FFD7C3",
             borderRadius: "10px",
             background: "#fff8ef",
           }}
+          onSubmit={handleSubmit}
         >
-          <div className="text-xl font-yg-jalnan text-yellow-900 mb-9">회원정보 입력</div>
-          <input type="text" placeholder="이메일 주소 입력" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full h-[2.8125rem] mb-9 text-[0.875rem] p-[0.75rem] rounded-[10px] border-solid border-[1px] border-[#FFD7C3]" />
+          <div className="text-xl font-yg-jalnan text-yellow-900 mb-9">
+            회원정보 입력
+          </div>
+          <input
+            type="text"
+            placeholder="이름 입력"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full h-[2.8125rem] mb-9 text-[0.875rem] p-[0.75rem] rounded-[10px] border-solid border-[1px] border-[#FFD7C3]"
+          />
+          <input
+            type="email"
+            placeholder="이메일 주소 입력"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full h-[2.8125rem] mb-9 text-[0.875rem] p-[0.75rem] rounded-[10px] border-solid border-[1px] border-[#FFD7C3]"
+          />
           <input
             type="password"
             placeholder="8자리 이상 영문, 숫자, 특수문자 포함"
@@ -91,10 +112,13 @@ function SignUp() {
             onChange={(e) => setConfirmPassword(e.target.value)}
             className="w-full h-[2.8125rem] mb-9 text-[0.875rem] p-[0.75rem] rounded-[10px] border-solid border-[1px] border-[#FFD7C3]"
           />
-          <button onClick={handleSubmit} type="submit" className="w-full h-[2.8125rem] font-yg-jalnan text-white bg-[#FFD7C3] rounded-[10px] hover:bg-[#F99363]">
+          <button
+            type="submit"
+            className="w-full h-[2.8125rem] font-yg-jalnan text-white bg-[#FFD7C3] rounded-[10px] hover:bg-[#F99363]"
+          >
             회원가입 완료
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
