375c5db8aa16e42b55f729b139e20471b19203f7
v-va <poruch0915@gmail.com>
2024-05-27 01:14:15 +0900

    #58 feat : 토큰 전송

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import { getCookie } from "../utils/cookie";
 
+const token = getCookie("token");
 const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
   withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": "*",
+    Authorization: `${token}`,
   },
 });
 
